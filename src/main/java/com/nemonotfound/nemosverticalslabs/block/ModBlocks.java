a73f5b5da5fe325c564b5e3925b6be8f9bf3b8e7
NemoNotFound <02grimes-bard@icloud.com>
2024-10-22 19:12:29 +0200

    Refactoring

--- src/main/java/com/nemonotfound/nemosverticalslabs/block/ModBlocks.java
+++ src/main/java/com/nemonotfound/nemosverticalslabs/block/ModBlocks.java
@@ -3,8 +3,6 @@ package com.nemonotfound.nemosverticalslabs.block;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -146,9 +144,5 @@ public class ModBlocks {
 
     private static RegistryKey<Block> keyOf(String path) {
         return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(MOD_ID, path));
-    }
-
-    private static Block registerBlock(String path, Block block) {
-        return Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, path), block);
     }
 }
